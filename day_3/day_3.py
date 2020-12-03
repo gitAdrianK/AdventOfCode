ce0bfa4779f5b5c97fb430196c2ed06b853c5804
akrimpenfort <akrimpenfort@uni-osnabrueck.de>
2020-12-03 10:07:14 +0100

    add day 3 part 2 solution
    change the program to be more modular
    to avoid bloating the code and avoiding
    copy-paste errors

--- day_3/day_3.py
+++ day_3/day_3.py
@@ -1,11 +1,22 @@
-trees_encountered = 0
-current_pos = 0
+# Steps in the form (moves right, moves down)
+steps = (
+    (1,1),
+    (3,1),
+    (5,1),
+    (7,1),
+    (1,2),
+)
+trees_encountered = [0] * len(steps)
 f = open("input.txt", "r")
-for line in f.readlines():
-    if  line[current_pos] == '#':
-        trees_encountered += 1
-    current_pos += 3
-    # The length of a line is always 32 characters
-    # Since the trees repeat their pattern forever wrap around to the start
-    current_pos %= 31
+for line_counter, line in enumerate(f.readlines()):
+    for step_counter, step in enumerate(steps):
+        # Skip lines in case the "moves down" step is not 1
+        if step[1] != 1 and line_counter % step[1] == 0:
+            continue
+        if  line[line_counter * step[0] % 31] == '#':
+            trees_encountered[step_counter] += 1
 print(trees_encountered)
+result = 1
+for trees in trees_encountered:
+    result = result * trees 
+print(result) 
