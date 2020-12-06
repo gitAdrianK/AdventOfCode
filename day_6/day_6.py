17ad8fa72d65e7a6d51d4b5131457071aaaf6981
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-06 11:28:13 +0100

    add solution day 6 part 2

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -1,10 +1,23 @@
 f = open("input.txt", "r")
 group = ""
 count = 0
+# [1,2,3,4] ∩ [3,4,5,6] = [3,4]
+# Same name as the function but it is what it is
+intersection = None
+intersection_count = 0
 for line in f.readlines():
     if line == "\n":
         count += len(set(group))
         group = ""
+        intersection_count+= len(intersection)
+        intersection = None
+        continue
     group += line.replace("\n", "")
+    if intersection is None:
+        intersection = set(group)
+    else:
+        intersection = intersection.intersection(set(line.replace("\n", "")))
+# Again, last line leaves the loop, so we count it manually
 count += len(set(group))
-print(count)
+intersection_count+= len(intersection)
+print(count, intersection_count)
