7754b8367978267ebf2fe3a5b8ae9d778cd54759
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-06 10:36:16 +0100

    add problem day 6 part 2

--- day_6/day_6.py
+++ day_6/day_6.py
@@ -3,10 +3,8 @@ group = ""
 count = 0
 for line in f.readlines():
     if line == "\n":
-        print(len(set(group)))
         count += len(set(group))
         group = ""
     group += line.replace("\n", "")
-print(len(set(group)))
 count += len(set(group))
 print(count)
