b67589e0b9e2092b2dfa603341820fed59c7d312
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-08 12:21:18 +0100

    change solution day 8 part 1

--- day_8/src/main.rs
+++ day_8/src/main.rs
@@ -5,15 +5,18 @@ fn main() {
     let input = fs::read_to_string("input.txt").expect("Unable to read file");
     for line in input.lines() {
         let split: Vec<&str> = line.split(" ").collect();
-        operations.push((split[0].into(), split[1].parse().unwrap(), false));
+        operations.push((split[0], split[1].parse().unwrap(), false));
     }
     let mut accumulator = 0;
     // While adress can have negative numbers, we trust it to never
     // actually be negative as this would adress nothing
     // and the problem speak of an infinite loop
     let mut adress = 0;
-    while operations.get(adress as usize).unwrap().2 == false {
-        let current_op = operations.get_mut(adress as usize).unwrap();
+    while let Some(current_op) = operations.get_mut(adress as usize) {
+        if current_op.2 {
+            println!("Exited via attempted looping.");
+            break;
+        }
         match &current_op {
             ("nop", _, _) => adress = adress + 1,
             ("acc", acc, _) => {
