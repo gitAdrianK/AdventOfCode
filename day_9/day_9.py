543fb8e6a7f85e6f53676a9c7e1abf3f4bae91c3
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-09 10:59:38 +0100

    add solution day 9 part 2

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -9,8 +9,32 @@ def hasSum(numbers):
     return sum
 
 l = list()
+# Part 1 answer: 50047984
+l2 = list()
+hasFound = False
 f = open("input.txt", "r")
 for line in f.readlines():
+    # Part 2
+    if not hasFound:
+        l2.append(int(line))
+        l2_sum = sum(l2)
+        # Check if just adding a new number to the list gets the result
+        if l2_sum == 50047984:
+            print(min(l2) + max(l2))
+            hasFound = True
+        # If the number was too large take from the front until smaller
+        while l2_sum > 50047984:
+            l2.pop(0)
+            l2_sum = sum(l2)
+            # Check if just removing a number from the list gets the result
+            if l2_sum == 50047984:
+                print(min(l2) + max(l2))
+                hasFound = True
+    # Since the numbers grow larger the sum of consecutive numbers can
+    # only contain numbers from before the part 1 result has been found.
+    # That means by the time part 1 finds its result and breaks the loop
+    # we will already have found a solution to part 2
+    # Part 1
     if len(l) == 26:
         result = hasSum(l)
         l.pop(0)
