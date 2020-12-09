377cadf5494574781bf6891b410bc3fd019288df
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-09 11:51:21 +0100

    optimize solution day 9 part 1

--- day_9/day_9.py
+++ day_9/day_9.py
@@ -8,6 +8,33 @@ def hasSum(numbers):
                 return -1
     return sum
 
+# For a list as small as 25 this function is not faster
+# However if we change
+#   if len(l) == 26:
+# to
+#   if len(l) == 220:
+# We can notice a speed increase
+# Using cProfile:
+# 26 Elements
+#   hasSum      =>  3880 function calls in 0.008 seconds
+#   hasSumAlt   =>  4916 function calls in 0.008 seconds
+# 220 Elements
+#   hasSum      =>  3492 function calls in 1.398 seconds
+#   hasSumAlt   =>  4140 function calls in 0.032 seconds
+def hasSumAlt(numbers):
+    sum = numbers[-1]
+    sorted_numbers = sorted(numbers[:-1])
+    left = 0
+    right = len(sorted_numbers) - 1
+    while left < right:
+        if (sorted_numbers[left] + sorted_numbers[right] == sum):
+            return -1
+        elif (sorted_numbers[left] + sorted_numbers[right] < sum):
+            left += 1
+        else:
+            right -= 1
+    return sum
+
 l = list()
 # Part 1 answer: 50047984
 l2 = list()
@@ -36,7 +63,7 @@ for line in f.readlines():
     # we will already have found a solution to part 2
     # Part 1
     if len(l) == 26:
-        result = hasSum(l)
+        result = hasSumAlt(l)
         l.pop(0)
         l.append(int(line))
         if result > 0:
