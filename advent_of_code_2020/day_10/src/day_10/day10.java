fee03787566d8d35957b4db893927f8e1d6a28b5
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-10 19:31:30 +0100

    rewrite traverseGraph method
    this solution is closer to how I started
    solving day 10 p2, however abandoned in favour
    of recursion w/ memoisation since it was
    simper to wrap my head around

--- advent_of_code_2020/day_10/src/day_10/day10.java
+++ advent_of_code_2020/day_10/src/day_10/day10.java
@@ -5,13 +5,10 @@ import java.io.FileNotFoundException;
 import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 import java.util.Scanner;
 
 public class day10 {
-
 	public static void main(String[] args) throws FileNotFoundException {
 		solveDay10("test_input.txt");
 		solveDay10("test_input_2.txt");
@@ -19,8 +16,6 @@ public class day10 {
 	}
 
 	private static void solveDay10(String input) throws FileNotFoundException {
-		int diff1 = 0;
-		int diff3 = 1;
 		List<Integer> numbers = new ArrayList<Integer>();
 		Scanner sc = new Scanner(new File(input));
 		// Charging outlet
@@ -31,6 +26,8 @@ public class day10 {
 		sc.close();
 		// Part 1
 		Collections.sort(numbers);
+		int diff1 = 0;
+		int diff3 = 1;
 		for (int i = 0; i < numbers.size() - 1; i++) {
 			if (numbers.get(i + 1) - numbers.get(i) == 1) {
 				diff1++;
@@ -42,29 +39,23 @@ public class day10 {
 		BigInteger arrange = traverseGraph(numbers);
 		System.out.println(input + ", " + diff1 * diff3 + ", " + arrange);
 	}
-
-	private static BigInteger traverseGraph(List<Integer> graph) {
-		return traverseGraph(graph, 0, new HashMap<Integer, BigInteger>());
-	}
-
-	private static BigInteger traverseGraph(List<Integer> graph, int start, Map<Integer, BigInteger> cache) {
-		if (cache.containsKey(start)) {
-			return cache.get(start);
-		}
-		BigInteger sum = BigInteger.valueOf(0);
-		if (start == graph.size() - 1) {
-			return BigInteger.valueOf(1);
-		}
-		for (int i = 1; i <= 3; i++) {
-			if (start + i > graph.size() - 1) {
-				break;
+	
+	private static BigInteger traverseGraph(List<Integer> arr) {
+		ArrayList<BigInteger> cache = new ArrayList<BigInteger>();
+		cache.add(BigInteger.valueOf(1));
+		for (int i = 1; i < arr.size(); i++) {
+			BigInteger sum = BigInteger.valueOf(0);
+			for (int j = 1; j <= 3; j++) {
+				// Out of bounds protection
+				if (i - j < 0) {
+					continue;
+				}
+				if (arr.get(i) - arr.get(i - j) <= 3) {
+					sum = sum.add(cache.get(i - j));
+				}
 			}
-			if (graph.get(start + i) - graph.get(start) > 3) {
-				break;
-			}
-			sum = sum.add(traverseGraph(graph, start + i, cache));
+			cache.add(sum);
 		}
-		cache.put(start, sum);
-		return sum;
+		return cache.get(cache.size() - 1);
 	}
 }
