b2fe92f50fa3c1bc3ec8d5f0c4ba24c48d3f5d4b
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-10 12:47:15 +0100

    add solution day 10 part 2

--- advent_of_code_2020/day_10/src/day_10/day10.java
+++ advent_of_code_2020/day_10/src/day_10/day10.java
@@ -2,25 +2,21 @@ package day_10;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.Scanner;
 
 public class day10 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		if (solveDay10("test_input.txt") != 35) {
-			System.out.println("Failed test_input");
-			return;
-		}
-		if (solveDay10("test_input_2.txt") != 220) {
-			System.out.println("Failed test_input_2");
-			return;
-		}
-		System.out.println(solveDay10("input.txt"));
+		solveDay10("test_input.txt");
+		solveDay10("test_input_2.txt");
+		solveDay10("input.txt");
 	}
 
-	private static int solveDay10(String input) throws FileNotFoundException {
+	private static void solveDay10(String input) throws FileNotFoundException {
 		int diff1 = 0;
 		int diff3 = 1;
 		ArrayList<Integer> numbers = new ArrayList<Integer>();
@@ -31,6 +27,7 @@ public class day10 {
 			numbers.add(sc.nextInt());
 		}
 		sc.close();
+		// Part 1
 		Collections.sort(numbers);
 		for (int i = 0; i < numbers.size() - 1; i++) {
 			if (numbers.get(i + 1) - numbers.get(i) == 1) {
@@ -39,7 +36,29 @@ public class day10 {
 				diff3++;
 			}
 		}
-		return diff1 * diff3;
+		BigInteger arrange = traverseGraph(0, numbers, new HashMap<Integer, BigInteger>());
+		System.out.println(input + ", " + diff1 * diff3 + ", " + arrange);
+	}
+
+	private static BigInteger traverseGraph(int start, ArrayList<Integer> graph, HashMap<Integer, BigInteger> cache) {
+		if (cache.containsKey(start)) {
+			return cache.get(start);
+		}
+		BigInteger sum = BigInteger.valueOf(0);
+		if (start == graph.size() - 1) {
+			return BigInteger.valueOf(1);
+		}
+		for (int i = 1; i <= 3; i++) {
+			if (start + i > graph.size() - 1) {
+				break;
+			}
+			if (graph.get(start + i) - graph.get(start) > 3) {
+				break;
+			}
+			sum = sum.add(traverseGraph(start + i, graph, cache));
+		}
+		cache.put(start, sum);
+		return sum;
 	}
 
 }
