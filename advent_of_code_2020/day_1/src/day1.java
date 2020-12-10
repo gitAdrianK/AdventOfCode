c5dd827d690a85680f05bfdd8ee235edbaf1f8e2
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-10 02:12:38 +0100

    rewrite day 1 to use scanner

--- advent_of_code_2020/day_1/src/day1.java
+++ advent_of_code_2020/day_1/src/day1.java
@@ -1,37 +1,32 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.List;
+import java.util.Scanner;
 
 public class day1 {
-	public static void main(String args[]) {
-		List<Integer> numbers = new ArrayList<Integer>();
-		// Read all numbers of the file and add them to a list
-		try {
-			BufferedReader reader = new BufferedReader(new FileReader("input.txt"));
-			String line;
-			while ((line = reader.readLine()) != null) {
-				numbers.add(Integer.parseInt(line));
-			}
-			reader.close();
-		} catch (Exception e) {
-			System.err.format("Exception occurred trying to read '%s'.", "input.txt");
-			e.printStackTrace();
-			return;
+	public static void main(String args[]) throws FileNotFoundException {
+		ArrayList<Integer> numbers = new ArrayList<Integer>();
+		Scanner sc = new Scanner(new File("input.txt"));
+		while (sc.hasNextInt()) {
+			numbers.add(sc.nextInt());
 		}
+		sc.close();
 		// Add all pairs of numbers together and print out the result of the
 		// multiplication should the addition result in 2020
+		int p1 = 0;
+		int p2 = 0;
 		for (int a : numbers) {
 			for (int b : numbers) {
-				if (a + b == 2020) {
-					System.out.println(a * b);
+				if (p1 == 0 && a + b == 2020) {
+					p1 = a * b;
 				}
 				for (int c : numbers) {
-					if (a + b + c == 2020) {
-						System.out.println(a * b * c);
+					if (p2 == 0 && a + b + c == 2020) {
+						p2 = a * b * c;
 					}
 				}
 			}
 		}
+		System.out.println(p1 + " " + p2);
 	}
 }
