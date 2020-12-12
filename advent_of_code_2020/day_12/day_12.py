fe136e3bd64ee661007f1e9bb8df2b2a7596f155
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-12 12:30:38 +0100

    add solution day 12 part 2

--- advent_of_code_2020/day_12/day_12.py
+++ advent_of_code_2020/day_12/day_12.py
@@ -1,4 +1,8 @@
+import numpy as np
 from enum import IntFlag
+
+from numpy.core.einsumfunc import _update_other_results
+
 
 class Cardinal(IntFlag):
     NORTH = 0
@@ -6,12 +10,14 @@ class Cardinal(IntFlag):
     SOUTH = 180
     WEST = 270
 
+
 class Sagittal(IntFlag):
     LEFT = -1
     RIGHT = 1
 
+
 class Ferry:
-    def __init__(self) -> None:
+    def __init__(self):
         self.pos_x = 0
         self.pos_y = 0
         self.facing = Cardinal.EAST
@@ -26,20 +32,53 @@ class Ferry:
         elif cardinal is Cardinal.WEST:
             self.pos_x -= amount
 
-    def turn(self, sagittal, amount):
+    def move_by_waypoint(self, waypoint):
+        self.pos_x += waypoint.pos_x
+        self.pos_y += waypoint.pos_y
+
+    def rotate(self, sagittal, amount):
         amount *= sagittal
         self.facing = Cardinal((self.facing + amount) % 360)
+
+
+class Waypoint:
+    def __init__(self):
+        self.pos_x = 10
+        self.pos_y = 1
+
+    def move(self, cardinal, amount):
+        if cardinal is Cardinal.NORTH:
+            self.pos_y += amount
+        elif cardinal is Cardinal.EAST:
+            self.pos_x += amount
+        elif cardinal is Cardinal.SOUTH:
+            self.pos_y -= amount
+        elif cardinal is Cardinal.WEST:
+            self.pos_x -= amount
+
+    def rotate(self, sagittal, amount):
+        if sagittal is Sagittal.RIGHT:
+            amount *= -1
+        theta = np.radians(amount)
+        c, s = int(np.cos(theta)), int(np.sin(theta))
+        rot_matrix = np.array(((c, -s), (s, c)))
+        pos_vec = np.array((self.pos_x, self.pos_y))
+        result = np.matmul(rot_matrix, pos_vec)
+        self.pos_x, self.pos_y = result[0], result[1]
+
 
 def solveDay12(input):
     # Setup
     ferry = Ferry()
     f = open(input, "r")
-    for line in f.readlines():
+    lines = list(l for l in f.readlines())
+    # Part 1
+    for line in lines:
         var = line[0]
         if var == "L":
-            ferry.turn(Sagittal.LEFT, int(line[1:]))
+            ferry.rotate(Sagittal.LEFT, int(line[1:]))
         elif var == "R":
-            ferry.turn(Sagittal.RIGHT, int(line[1:]))
+            ferry.rotate(Sagittal.RIGHT, int(line[1:]))
         elif var == "N":
             ferry.move(Cardinal.NORTH, int(line[1:]))
         elif var == "E":
@@ -50,10 +89,30 @@ def solveDay12(input):
             ferry.move(Cardinal.WEST, int(line[1:]))
         elif var == "F":
             ferry.move(ferry.facing, int(line[1:]))
-    # Part 1
     p1 = abs(ferry.pos_x) + abs(ferry.pos_y)
-    return (p1, 0)
+    # Part 2
+    ferry = Ferry()
+    waypoint = Waypoint()
+    for line in lines:
+        var = line[0]
+        if var == "L":
+            waypoint.rotate(Sagittal.LEFT, int(line[1:]))
+        elif var == "R":
+            waypoint.rotate(Sagittal.RIGHT, int(line[1:]))
+        elif var == "N":
+            waypoint.move(Cardinal.NORTH, int(line[1:]))
+        elif var == "E":
+            waypoint.move(Cardinal.EAST, int(line[1:]))
+        elif var == "S":
+            waypoint.move(Cardinal.SOUTH, int(line[1:]))
+        elif var == "W":
+            waypoint.move(Cardinal.WEST, int(line[1:]))
+        elif var == "F":
+            for i in range(int(line[1:])):
+                ferry.move_by_waypoint(waypoint)
+    p2 = abs(ferry.pos_x) + abs(ferry.pos_y)
+    return (p1, p2)
 
 
-assert((25, 0) == solveDay12("test_input.txt"))
+assert((25, 286) == solveDay12("test_input.txt"))
 print(solveDay12("input.txt"))
