381f4fe27c4d1d756a52004414f276a597dcfb57
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-15 21:56:44 +0100

    clean up

--- advent_of_code_2020/day_02/src/main.rs
+++ advent_of_code_2020/day_02/src/main.rs
@@ -1,32 +1,28 @@
+extern crate regex;
+use regex::Regex;
 use std::fs;
 
 fn main() {
-    let mut valid_pw_total = 0;
-    let mut valid_pw_total_p2 = 0;
+    let re = Regex::new(r"([0-9]+)-([0-9]+) ([a-z]{1}): ([a-z]+)").unwrap();
     let data = fs::read_to_string("input.txt").expect("Unable to read file");
+    let (mut p1, mut p2) = (0, 0);
     for line in data.lines() {
-        let split_line: Vec<&str> = line.split(" ").collect();
-        let pw_req_amount: Vec<&str> = split_line[0].split("-").collect();
-        let pw_min_amount: u8 = pw_req_amount[0].parse().unwrap();
-        let pw_max_amount: u8 = pw_req_amount[1].parse().unwrap();
-        let pw_req_letter = split_line[1].chars().nth(0).unwrap();
-        // Part 1
-        let mut count = 0;
-        for c in split_line[2].chars() {
-            if c == pw_req_letter {
-                count = count + 1;
-            }
-        }
-        if pw_min_amount <= count && count <= pw_max_amount {
-            valid_pw_total = valid_pw_total + 1;
-        }
-        // Part 2
-        // Reusing pw_min_amount and pw_max_amount w/o changing the names.
-        // Better names would be pw_pos_one and pw_pos_two
-        if (split_line[2].chars().nth((pw_min_amount - 1).into()).unwrap() == pw_req_letter)
-            ^ (split_line[2].chars().nth((pw_max_amount - 1).into()).unwrap() == pw_req_letter) {
-                valid_pw_total_p2 = valid_pw_total_p2 + 1;
-        }
+        let cap = re.captures(line).unwrap();
+        let a = cap[1].parse().unwrap();
+        let b = cap[2].parse().unwrap();
+        let c = cap[3].chars().nth(0).unwrap();
+        let d = &cap[4];
+        if solve_part_1(a, b, c, d) { p1 += 1; }
+        if solve_part_2(a, b, c, d) { p2 += 1; }
     }
-    println!("{:?}, {:?}", valid_pw_total, valid_pw_total_p2);
+    println!("{:?}, {:?}", p1, p2);
+}
+
+fn solve_part_1(min: usize, max: usize, letter: char, pw: &str) -> bool {
+    let c = pw.matches(letter).count();
+    min <= c && c <= max
+}
+
+fn solve_part_2(pos1: usize, pos2: usize, letter: char, pw: &str) -> bool {
+    (pw.chars().nth(pos1 - 1).unwrap() == letter) ^ (pw.chars().nth(pos2 - 1).unwrap() == letter)
 }
