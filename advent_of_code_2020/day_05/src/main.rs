57e24b874c91781b4f419a5951c8719ce27fb3ad
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-15 23:09:00 +0100

    clean up

--- advent_of_code_2020/day_05/src/main.rs
+++ advent_of_code_2020/day_05/src/main.rs
@@ -4,37 +4,26 @@ fn main() {
     let mut seats: Vec<u32> = Vec::new();
     let data = fs::read_to_string("input.txt").expect("Unable to read file");
     for input in data.lines() {
-        let id = find_seat_id(input);
-        seats.push(id);
+        seats.push(find(&input[..7], 0, 127) * 8 + find(&input[7..input.len()], 0, 7));
     }
     seats.sort();
-    println!("Highest Id: {}", seats.last().unwrap());
-    let mut my_seat = *seats.first().clone().unwrap();
+    let mut my_seat = *seats.first().unwrap();
     for &seat in seats.iter() {
-        if seat != my_seat {
-            break
-        }
-        my_seat = my_seat + 1;
+        if seat != my_seat { break }
+        my_seat += 1;
     }
-    println!("My seat: {}", my_seat);
+    println!("Highest Id: {} \nMy seat: {}", seats.last().unwrap(), my_seat);
 }
 
-fn find_seat_id(input: &str) -> u32 {
-    fn find_binarily(input: &str, lower_bound: u32, upper_bound: u32) -> u32 {
-        let character = match input.chars().nth(0) {
-            Some(character) => character,
-            None => return upper_bound,
-        };
-        let diff = (upper_bound - lower_bound) / 2;
-        match character {
-            'F' | 'L' => {
-                return find_binarily(&input[1..], lower_bound, upper_bound - diff - 1)
-            },
-            'B' | 'R' => {
-                return find_binarily(&input[1..], lower_bound + diff + 1, upper_bound)
-            },
+fn find(input: &str, lo: u32, hi: u32) -> u32 {
+    let (mut hi, mut lo, mut step) = (hi, lo, hi / 2);
+    for c in input.chars() {
+        match c {
+            'F' | 'L' => hi = hi - step - 1,
+            'B' | 'R' => lo = lo + step + 1,
             _ => unreachable!(),
         }
+        step = step / 2;
     }
-    return find_binarily(&input[..7], 0, 127) * 8 + find_binarily(&input[7..input.len()], 0, 7)
+    hi
 }
