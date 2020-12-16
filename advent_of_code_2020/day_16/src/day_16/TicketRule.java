1e800e8d72edf94b75c2ad2026e7179855f0e3cf
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-16 15:33:04 +0100

    add solution day 16 part 2

--- advent_of_code_2020/day_16/src/day_16/TicketRule.java
+++ advent_of_code_2020/day_16/src/day_16/TicketRule.java
@@ -1,12 +1,16 @@
 package day_16;
 
+import java.util.Comparator;
+
 import javafx.util.Pair;
 
 public class TicketRule {
-	Pair<Integer, Integer> lowerRange;
-	Pair<Integer, Integer> higherRange;
+	private String name;
+	private Pair<Integer, Integer> lowerRange;
+	private Pair<Integer, Integer> higherRange;
 
-	public TicketRule(int loLo, int loHi, int hiLo, int hiHi) {
+	public TicketRule(String name, int loLo, int loHi, int hiLo, int hiHi) {
+		this.name = name;
 		this.lowerRange = new Pair<Integer, Integer>(loLo, loHi);
 		this.higherRange = new Pair<Integer, Integer>(hiLo, hiHi);
 	}
@@ -15,9 +19,34 @@ public class TicketRule {
 		return (number >= lowerRange.getKey() && number <= lowerRange.getValue())
 				|| (number >= higherRange.getKey() && number <= higherRange.getValue());
 	}
-	
+
+	public boolean isValidForAll(int[] numbers) {
+		for (int n : numbers) {
+			if (!isValid(n)) {
+				return false;
+			}
+		}
+		return true;
+	}
+
+	public String getName() {
+		return name;
+	}
+
 	@Override
 	public String toString() {
-		return lowerRange.getKey() + "-" + lowerRange.getValue() + ", " + higherRange.getKey() + "-" + higherRange.getValue();
+		return name;
+	}
+	
+//	@Override
+//	public String toString() {
+//		return name + ": " + lowerRange.getKey() + "-" + lowerRange.getValue() + ", " + higherRange.getKey() + "-"
+//				+ higherRange.getValue();
+//	}
+}
+
+class NameCompare implements Comparator<TicketRule> {
+	public int compare(TicketRule t1, TicketRule t2) {
+		return t1.getName().compareTo(t2.getName());
 	}
 }
--- advent_of_code_2020/day_16/src/day_16/day16.java
+++ advent_of_code_2020/day_16/src/day_16/day16.java
@@ -3,34 +3,38 @@ package day_16;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class day16 {
 
 	public static void main(String[] args) throws FileNotFoundException {
-		solveDay16("test_input.txt");
+		// solveDay16("test_input.txt");
+		// solveDay16("test_input_2.txt");
 		solveDay16("input.txt");
 	}
 
 	public static void solveDay16(String input) throws FileNotFoundException {
 		int p1 = 0;
+
+		// Rules and tickets
 		ArrayList<TicketRule> rules = new ArrayList<TicketRule>();
-		int[] yourTicket;
+		int[] yourTicket = null;
+		ArrayList<int[]> nearbyTickets = new ArrayList<int[]>();
+
 		Scanner sc = new Scanner(new File(input));
-		for (int i = 0; sc.hasNextLine(); i = i) {
+		for (int i = 0; sc.hasNextLine();) {
 			String line = sc.nextLine();
-			// Newline
-			if (line.isEmpty()) {
-				i++;
-				continue;
-			}
-			// Non number containing lines
+			// Newline & Non number containing lines
 			if (!line.matches(".*[0-9]+.*")) {
+				i++;
 				continue;
 			}
 			switch (i) {
 			case 0:
 				// Ticket Rules
+				String name = line.replaceAll(":.*", "");
 				line = line.replaceAll("[^0-9]+", " ");
 				line = line.trim();
 				String[] ruleStr = line.split("[^0-9]+");
@@ -38,34 +42,20 @@ public class day16 {
 				for (int j = 0; j < ruleStr.length; j++) {
 					ruleInt[j] = Integer.parseInt(ruleStr[j]);
 				}
-				rules.add(new TicketRule(ruleInt[0], ruleInt[1], ruleInt[2], ruleInt[3]));
-				break;
-			case 1:
-				// Your Ticket
-				String[] yourTicketStr = line.split(",");
-				int[] yourTicketInt = new int[yourTicketStr.length];
-				for (int j = 0; j < yourTicketStr.length; j++) {
-					yourTicketInt[j] = Integer.parseInt(yourTicketStr[j]);
-				}
-				yourTicket = yourTicketInt;
+				rules.add(new TicketRule(name, ruleInt[0], ruleInt[1], ruleInt[2], ruleInt[3]));
 				break;
 			case 2:
+				// Your Ticket
+				yourTicket = parseTicket(line);
+				break;
+			case 4:
 				// Nearby Tickets
-				String[] nrbyTicketStr = line.split(",");
-				int[] nrbyTicketInt = new int[nrbyTicketStr.length];
-				for (int j = 0; j < nrbyTicketStr.length; j++) {
-					nrbyTicketInt[j] = Integer.parseInt(nrbyTicketStr[j]);
-				}
-				for (int j : nrbyTicketInt) {
-					boolean isValidForOne = false;
-					for (TicketRule r : rules) {
-						if (r.isValid(j)) {
-							isValidForOne = true;
-						}
-					}
-					if (!isValidForOne) {
-						p1 += j;
-					}
+				int[] ticket = parseTicket(line);
+				int errorValue = solvePart1(rules, ticket);
+				if (errorValue == -1) {
+					nearbyTickets.add(ticket);
+				} else {
+					p1 += errorValue;
 				}
 				break;
 			default:
@@ -73,6 +63,80 @@ public class day16 {
 			}
 		}
 		sc.close();
-		System.out.println(p1);
+		long p2 = solvePart2(rules, nearbyTickets, yourTicket);
+		System.out.println(p1 + " " + p2);
+	}
+
+	private static int solvePart1(List<TicketRule> rules, int[] nrbyTicketInt) {
+		for (int j : nrbyTicketInt) {
+			boolean isValidForOne = false;
+			for (TicketRule r : rules) {
+				if (r.isValid(j)) {
+					isValidForOne = true;
+				}
+			}
+			if (!isValidForOne) {
+				return j;
+			}
+		}
+		return -1;
+	}
+
+	private static long solvePart2(List<TicketRule> rules, List<int[]> nearbyTickets, int[] yourTicket) {
+		ArrayList<ArrayList<TicketRule>> allPossible = new ArrayList<ArrayList<TicketRule>>();
+		int[] column = new int[nearbyTickets.size() + 1];
+		for (int i = 0; i < yourTicket.length; i++) {
+			column[0] = yourTicket[i];
+			int index = 1;
+			for (int[] nearbyTicket : nearbyTickets) {
+				column[index] = nearbyTicket[i];
+				index++;
+			}
+			ArrayList<TicketRule> possible = new ArrayList<TicketRule>();
+			for (TicketRule rule : rules) {
+				if (rule.isValidForAll(column)) {
+					possible.add(rule);
+				}
+			}
+			allPossible.add(possible);
+		}
+		TicketRule[] assigned = eliminateOptions(allPossible);
+		long p2 = 1;
+		for(int i = 0; i < assigned.length; i++) {
+			if (assigned[i].getName().contains("departure")) {
+				p2 *= yourTicket[i];
+			}
+		}
+		return p2;
+	}
+
+	private static int[] parseTicket(String line) {
+		String[] ticketStr = line.split(",");
+		int[] ticketInt = new int[ticketStr.length];
+		for (int j = 0; j < ticketStr.length; j++) {
+			ticketInt[j] = Integer.parseInt(ticketStr[j]);
+		}
+		return ticketInt;
+	}
+
+	private static TicketRule[] eliminateOptions(ArrayList<ArrayList<TicketRule>> list) {
+		return eliminateOptions(list, new TicketRule[list.size()]);
+	}
+
+	private static TicketRule[] eliminateOptions(ArrayList<ArrayList<TicketRule>> list, TicketRule[] assigned) {
+		TicketRule onlyRule = null;
+		for (int i = 0; i < list.size(); i++) {
+			if (list.get(i).size() == 1) {
+				onlyRule = list.get(i).get(0);
+				assigned[i] = onlyRule;
+			}
+		}
+		if (onlyRule == null) {
+			return assigned;
+		}
+		for (List<TicketRule> entry : list) {
+			entry.remove(onlyRule);
+		}
+		return eliminateOptions(list, assigned);
 	}
 }
