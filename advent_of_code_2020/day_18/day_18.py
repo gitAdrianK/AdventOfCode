5579b2d1a18bbb08f150830884f3ae0c1b90a9bf
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-18 22:40:19 +0100

    add solution day 18 part 2
    this got really ugly and I did note
    what I should have done in a comment
    above the function bracketsToGroups.
    the keyword here is SHOULD

--- advent_of_code_2020/day_18/day_18.py
+++ advent_of_code_2020/day_18/day_18.py
@@ -1,19 +1,19 @@
-from os import closerange
-
-
 def solveDay18(input):
     # Setup
     f = open(input, "r")
     # Part 1
     p1 = 0
+    p2 = 0
     for line in list(f.readlines()):
         equation = line.replace("\n", "").split(" ")
         group = bracketsToGroups(equation)
-        p1 += doMath(group)
+        p1 += doPart1(group)
+        p2 += doPart2(group)
 
-    return (p1, 0)
+    return (p1, p2)
 
-
+# I should just parse the input make and make a cute AST, and it would be so easy
+# but noooo, "I already did that once for uni, lets do it w/o all that jazz"
 def bracketsToGroups(equation):
     group = []
     sub_group = []
@@ -46,24 +46,57 @@ def bracketsToGroups(equation):
             finalGroup.append(bracketsToGroups(element))
     return finalGroup
 
-
-def doMath(expression):
+def doPart1(expression):
+    # Start
     result = expression[0]
     if len(result) != 1:
-        result = str(doMath(result))
+        result = str(doPart1(result))
+    # Left to right evaluation
     for n, exp in enumerate(expression[1:], 1):
+        # If we find an operand add the right side to result
         if len(exp) == 1:
-            if "+" in exp[0] or "*" in exp[0]:
+            if "+" in exp or "*" in exp:
                 next_ = expression[n + 1]
                 if len(next_) == 1:
                     result = str(eval(result + exp + next_))
                 else:
-                    result = str(eval(result + exp + str(doMath(next_))))
+                    result = str(eval(result + exp + str(doPart1(next_))))
     return int(result)
 
+def doPart2(expression):
+    # Start
+    left = []
+    right = []
+    result = expression[0]
+    if len(result) != 1:
+        result = str(doPart2(result))
+    # Find "*" and place in left and right
+    foundMult = False
+    for exp in expression:
+        if len(exp) == 1:
+            if "*" in exp and not foundMult:
+                foundMult = True
+                continue
+            if foundMult:
+                right.append(exp)
+            else:
+                left.append(exp)
+        else:
+            if foundMult:
+                right.append(str(doPart2(exp)))
+            else:
+                left.append(str(doPart2(exp)))
+    if not foundMult:
+        leftStr = ""
+        for l in left:
+            leftStr += l
+        result = str(eval(leftStr))
+    else:
+        result = str(eval(str(doPart2(left)) + "*" + str(doPart2(right))))
+    return int(result)
 
-assert((26, 0) == solveDay18("test_input.txt"))
-assert((437, 0) == solveDay18("test_input_2.txt"))
-assert((12240, 0) == solveDay18("test_input_3.txt"))
-assert((13632, 0) == solveDay18("test_input_4.txt"))
+assert((26, 46) == solveDay18("test_input.txt"))
+assert((437, 1445) == solveDay18("test_input_2.txt"))
+assert((12240, 669060) == solveDay18("test_input_3.txt"))
+assert((13632, 23340) == solveDay18("test_input_4.txt"))
 print(solveDay18("input.txt"))
