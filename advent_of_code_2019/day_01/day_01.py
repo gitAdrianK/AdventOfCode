58f0d479342571c09ba5d51a466153724d5201d0
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-25 21:43:51 +0100

    rename method name
    I read a bit of pythons PEP 8 again

--- advent_of_code_2019/day_01/day_01.py
+++ advent_of_code_2019/day_01/day_01.py
@@ -1,17 +1,16 @@
-
-def solveDay01(input):
+def solve_day_01(input):
     p1 = 0
     p2 = 0
     f = open(input, "r")
     for mass in (int(mass) for mass in list(f.readlines())):
-        fuel = int(mass / 3) - 2
+        fuel = int(mass/3) - 2
         p1 += fuel
         p2 += fuel
         while fuel > 0:
-            fuel = int(fuel / 3) - 2
+            fuel = int(fuel/3) - 2
             if fuel > 0:
                 p2 += fuel
     return (p1, p2)
 
 
-print(solveDay01("input.txt"))
+print(solve_day_01("input.txt"))
