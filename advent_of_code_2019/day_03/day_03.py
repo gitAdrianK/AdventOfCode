544f7542f53050caf36baeb7bab57409a7341139
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2020-12-26 13:09:02 +0100

    add problem & solution day 3 part 2

--- advent_of_code_2019/day_03/day_03.py
+++ advent_of_code_2019/day_03/day_03.py
@@ -5,47 +5,69 @@ def solve_day_03(input):
     regex = re.compile("\w\d+")
     f = open(input, "r")
     cables = get_cables(regex.findall(f.readline()))
-    cable_one_horizontal = cables[0]
-    cable_one_vertical = cables[1]
+    one_horizontals = cables[0]
+    one_verticals = cables[1]
+    cable_one = cables[2]
     cables = get_cables(regex.findall(f.readline()))
-    cable_two_horizontal = cables[0]
-    cable_two_vertical = cables[1]
-    # print(cable_one_horizontal)
-    # print(cable_one_vertical)
-    # print(cable_two_horizontal)
-    # print(cable_two_vertical)
+    two_horizontals = cables[0]
+    two_verticals = cables[1]
+    cable_two = cables[2]
+    intersections = get_intersections(one_verticals, two_horizontals)
+    intersections.extend(get_intersections(two_verticals, one_horizontals))
     manhattan = float("inf")
-    for vertical in cable_one_vertical:
-        for horizontal in cable_two_horizontal:
-            if are_intersecting(vertical, horizontal):
-                m = get_manhattan(vertical, horizontal)
-                if m < manhattan:
-                    manhattan = m
-    for vertical in cable_two_vertical:
-        for horizontal in cable_one_horizontal:
-            if are_intersecting(vertical, horizontal):
-                m = get_manhattan(vertical, horizontal)
-                if m < manhattan:
-                    manhattan = m
+    signal_delay = float("inf")
+    for intersection in intersections:
+        m = get_manhattan(intersection)
+        if m < manhattan:
+            manhattan = m
+        delay_one = get_delay(cable_one, intersection)
+        delay_two = get_delay(cable_two, intersection)
+        s = delay_one + delay_two
+        if s < signal_delay:
+            signal_delay = s
+    return (manhattan, signal_delay)
 
-    return (manhattan, 0)
+
+def get_intersections(verticals, horizontals):
+    intersections = []
+    for vertical in verticals:
+        for horizontal in horizontals:
+            if are_intersecting(vertical, horizontal):
+                intersections.append((vertical[0][0], horizontal[0][1]))
+    return intersections
 
 
 def are_intersecting(vertical, horizontal):
-    if horizontal[0] == (0,0) and vertical[0] == (0,0):
+    if horizontal[0] == (0, 0) and vertical[0] == (0, 0):
         return False
     if vertical[0][1] <= horizontal[0][1] <= vertical[1][1]:
         if horizontal[0][0] <= vertical[0][0] <= horizontal[1][0]:
             return True
 
 
-def get_manhattan(vertical, horizontal):
-    return abs(vertical[0][0]) + abs(horizontal[0][1])
+def get_manhattan(point):
+    return abs(point[0]) + abs(point[1])
+
+
+def get_delay(cable, point):
+    delay = 0
+    for c in cable:
+        if c[0][0] <= point[0] <= c[1][0] or c[0][0] >= point[0] >= c[1][0]:
+            if c[0][1] <= point[1] <= c[1][1] or c[0][1] >= point[1] >= c[1][1]:
+                x_delay = abs(c[0][0] - point[0])
+                y_delay = abs(c[0][1] - point[1])
+                delay += x_delay + y_delay
+                return delay
+        x_delay = abs(c[0][0] - c[1][0])
+        y_delay = abs(c[0][1] - c[1][1])
+        delay += x_delay + y_delay
+    return float("inf")
 
 
 def get_cables(cables):
     horizontal = []
     vertical = []
+    combined = []
     point = (0, 0)
     prev_point = (0, 0)
     for cable in cables:
@@ -54,20 +76,24 @@ def get_cables(cables):
         if direction == "U":
             point = (point[0], point[1]+amount)
             vertical.append((prev_point, point))
+            combined.append((prev_point, point))
             prev_point = point
         elif direction == "D":
             point = (point[0], point[1]-amount)
             vertical.append((point, prev_point))
+            combined.append((prev_point, point))
             prev_point = point
         elif direction == "L":
             point = (point[0]-amount, point[1])
             horizontal.append((point, prev_point))
+            combined.append((prev_point, point))
             prev_point = point
         elif direction == "R":
             point = (point[0]+amount, point[1])
             horizontal.append((prev_point, point))
+            combined.append((prev_point, point))
             prev_point = point
-    return (horizontal, vertical)
+    return (horizontal, vertical, combined)
 
 
 print(solve_day_03("test_input_0.txt"))
