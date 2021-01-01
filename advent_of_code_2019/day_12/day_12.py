9b0a195989be227cea268a4f1123a3c94ebc5f2b
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-01-01 23:01:55 +0100

    add solution day 12 part 2
    not really happy with this one as I still don't
    quite know why my own attempt didn't work,
    also this solution has a fixed amount of moons

--- advent_of_code_2019/day_12/day_12.py
+++ advent_of_code_2019/day_12/day_12.py
@@ -1,82 +1,70 @@
-from itertools import permutations, count
+from itertools import permutations
+from functools import reduce
 from math import lcm
 import re
 
 
-class Planet:
-
-    x = None
-    y = None
-    z = None
-    vx = None
-    vy = None
-    vz = None
-
+class Moon:
     def __init__(self, x, y, z):
-        self.x = x
-        self.y = y
-        self.z = z
-        self.vx = 0
-        self.vy = 0
-        self.vz = 0
+        self.start = [x, y, z]
+        self.pos = self.start[:]
+        self.loop = [None, None, None]
+        self.vel = [0, 0, 0]
+        self.iter = 1
 
     def apply_gravity(self, other):
-        self.vx += sorted([-1, other.x-self.x, 1])[1]
-        self.vy += sorted([-1, other.y-self.y, 1])[1]
-        self.vz += sorted([-1, other.z-self.z, 1])[1]
+        for i in range(3):
+            self.vel[i] += sorted([-1, other.pos[i]-self.pos[i], 1])[1]
 
     def apply_velocity(self):
-        self.x += self.vx
-        self.y += self.vy
-        self.z += self.vz
+        self.iter += 1
+        for i in range(3):
+            self.pos[i] += self.vel[i]
 
     def get_energy(self):
-        return (abs(self.x)+abs(self.y)+abs(self.z)) * (abs(self.vx)+abs(self.vy)+abs(self.vz))
+        pot = 0
+        kin = 0
+        for i in range(3):
+            pot += abs(self.pos[i])
+            kin += abs(self.vel[i])
+        return pot*kin
 
 
 def solve_day_12(input, energy_at):
-    planets = []
-    start = []
-    looping = []
+    moons = []
     regex = re.compile("-{0,1}\d+")
     f = open(input, "r")
     for line in f.readlines():
         xyz = regex.findall(line)
-        planets.append(Planet(int(xyz[0]), int(xyz[1]), int(xyz[2])))
-        start.append(Planet(int(xyz[0]), int(xyz[1]), int(xyz[2])))
-        looping.append([0, 0, 0])
+        moons.append(Moon(int(xyz[0]), int(xyz[1]), int(xyz[2])))
     p1 = 0
-    for i in count(start=1):
-        for p in permutations(planets, 2):
+    lcms = {}
+    i = 0
+    while len(lcms) < 3:
+        for p in permutations(moons, 2):
             p[0].apply_gravity(p[1])
-        for j, planet in enumerate(planets):
+        for planet in moons:
             planet.apply_velocity()
-            loop = looping[j]
-            if planet.x == start[j].x and planet.vx == start[j].vx and loop[0] == 0:
-                loop[0] = i
-            if planet.y == start[j].y and planet.vy == start[j].vy and loop[1] == 0:
-                loop[1] = i
-            if planet.z == start[j].z and planet.vz == start[j].vz and loop[2] == 0:
-                loop[2] = i
+        i += 1
         if i == energy_at:
-            for planet in planets:
+            for planet in moons:
                 p1 += planet.get_energy()
-        loops_found = True
-        for loop in looping:
-            if loop.count(0) != 0:
-                loops_found = False
-                break
-        if p1 != 0 and loops_found:
-            break
-    flatten = []
-    for loop in looping:
-        for l in loop:
-            flatten.append(l)
-    while len(flatten) > 1:
-        flatten.append(lcm(flatten[0], flatten[1]))
-        flatten = flatten[2:]
-    p2 = flatten[0]
+        for j in range(3):
+            # Stolen from https://gitlab.com/scul/Advent-of-Code-2019/blob/master/day12/12.py
+            # My own attempt found the right periods but messed up the lcms somehow
+            if moons[0].pos[j] == moons[0].start[j] and \
+                    moons[1].pos[j] == moons[1].start[j] and \
+                    moons[2].pos[j] == moons[2].start[j] and \
+                    moons[3].pos[j] == moons[3].start[j] and \
+                    j not in lcms:
+                l = lcm_([m.iter for m in moons])
+                lcms[j] = l
+    p2 = lcm_(l for l in lcms.values())
     return (p1, p2)
+
+
+def lcm_(lst):
+    return reduce(lcm, lst)
 
 
 print(solve_day_12("test_input_0.txt", 10))
