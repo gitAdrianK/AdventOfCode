d3f54bb6bb90047459bcac5b6296146fd8d02991
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-01-07 11:30:26 +0100

    optimization changes
    it still isn't enough to do part 2

--- advent_of_code_2019/day_16/day_16.py
+++ advent_of_code_2019/day_16/day_16.py
@@ -2,19 +2,20 @@ def solve_day_16(input):
     pattern = [0, 1, 0, -1]
     f = open(input, "r")
     input = [int(c) for c in list(f.readline().replace("\n", ""))]
+    #input *= 10_000
     output = [0]*len(input)
     for _ in range(100):
         for i in range(len(input)):
-            p = [nr for slst in [[pt]*(i+1) for pt in pattern] for nr in slst]
             new = 0
-            for j, k in enumerate(input, 1):
-                new += (p[j % len(p)])*k
-            output[i] = int(list(str(new))[-1])
+            for j, k in enumerate(input[i:], i+1):
+                new += pattern[int(j/(i+1)) % len(pattern)]*k
+            output[i] = abs(new) % 10
         input = output
-    return ("".join(str(nr) for nr in output[:8]), 0)
+    p1 = int("".join(str(nr) for nr in output[:8]))
+    return (p1, input[p1:p1+8])
 
 
-# print(solve_day_16("test_input_0.txt"))
-# print(solve_day_16("test_input_1.txt"))
-# print(solve_day_16("test_input_2.txt"))
+print(solve_day_16("test_input_0.txt"))
+print(solve_day_16("test_input_1.txt"))
+print(solve_day_16("test_input_2.txt"))
 print(solve_day_16("input.txt"))
