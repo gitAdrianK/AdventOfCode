a93a48b5f6cec88303ab4501bb27f27dca52db98
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-01-08 22:25:04 +0100

    further optimization changes
    it still isn't enough for part 2 though

--- advent_of_code_2019/day_16/day_16.py
+++ advent_of_code_2019/day_16/day_16.py
@@ -1,21 +1,37 @@
 def solve_day_16(input):
     pattern = [0, 1, 0, -1]
+    pattern_length = 4
     f = open(input, "r")
     input = [int(c) for c in list(f.readline().replace("\n", ""))]
-    #input *= 10_000
-    output = [0]*len(input)
+    #input *= 10_000     # Disable for p1 result
+    length = len(input)
+    half = length//2
+    third = length//3
+    output = [0]*length
     for _ in range(100):
-        for i in range(len(input)):
-            new = 0
-            for j, k in enumerate(input[i:], i+1):
-                new += pattern[int(j/(i+1)) % len(pattern)]*k
-            output[i] = abs(new) % 10
+        output[-1] = input[-1]
+        for i in range(length-2, -1, -1):
+            if i >= half:
+                output[i] = (input[i]+output[i+1]) % 10
+            elif i >= third:
+                try:
+                    output[i] = input[i]+output[i+1]-input[i+i+1]-input[i+i+2]
+                except IndexError:
+                    output[i] = input[i]+output[i+1]-input[i+i+1]
+                output[i] = output[i] % 10
+            else:
+                new = 0
+                for j, k in enumerate(input[i:], i+1):
+                    new += pattern[j//(i+1) % pattern_length]*k
+                output[i] = abs(new) % 10
         input = output
-    p1 = int("".join(str(nr) for nr in output[:8]))
-    return (p1, input[p1:p1+8])
+        output = [0]*length
+    p1 = "".join(str(nr) for nr in input[:8])
+    p2 = "".join(str(nr) for nr in input[int(p1):int(p1)+8])
+    return (p1, p2)
 
 
 print(solve_day_16("test_input_0.txt"))
-print(solve_day_16("test_input_1.txt"))
-print(solve_day_16("test_input_2.txt"))
+# print(solve_day_16("test_input_1.txt"))
+# print(solve_day_16("test_input_2.txt"))
 print(solve_day_16("input.txt"))
