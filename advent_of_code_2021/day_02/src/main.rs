09f2c5ecc9ca91ddac688855d992c32c28f2ae3f
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-12-02 10:28:24 +0100

    Add problem and solution day 02 part 2

--- advent_of_code_2021/day_02/src/main.rs
+++ advent_of_code_2021/day_02/src/main.rs
@@ -1,20 +1,36 @@
 use std::fs;
 
 fn main() {
+    // Part 1
     let mut h = 0;
     let mut v = 0;
 
-    let data = fs::read_to_string("test_input.txt").expect("Unable to read file");
+    // Part 2
+    let mut h2 = 0;
+    let mut v2 = 0;
+    let mut aim = 0;
+
+    let data = fs::read_to_string("input.txt").expect("Unable to read file");
     for line in data.lines() {
         let line_split: Vec<&str> = line.split(" ").collect();
         let command = line_split[0];
         let amount = line_split[1].parse::<u32>().unwrap();
         match command {
-            "forward" => v = v + amount,
-            "down" => h = h + amount,
-            "up" => h = h - amount,
+            "forward" => {
+                v = v + amount;
+                h2 = h2 + amount;
+                v2 = v2 + aim * amount;
+            },
+            "down" => {
+                h = h + amount;
+                aim = aim + amount;
+            },
+            "up" => {
+                h = h - amount;
+                aim = aim - amount;
+            },
             _ => unreachable!(),
         }
     }
-    println!("{:?}", h*v);
+    println!("{:?}, {:?}", h*v, h2*v2);
 }
