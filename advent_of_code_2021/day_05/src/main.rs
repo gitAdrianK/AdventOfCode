634815c1a29d3c9b41515371ea74fb3cdd8ebc1e
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-12-05 16:55:57 +0100

    Add problem and solution day 05 part 2
    Improve way to get coordinates significantly, because I
    remembered the better way.

--- advent_of_code_2021/day_05/src/main.rs
+++ advent_of_code_2021/day_05/src/main.rs
@@ -3,45 +3,44 @@ use std::fs;
 
 fn main() {
     let data = fs::read_to_string("input.txt").expect("Unable to read file");
-    let mut positions: HashMap<(u32, u32), u32> = HashMap::new();
+    let mut positions: HashMap<(i32, i32), u32> = HashMap::new();
     for line in data.lines() {
         let from_to_split: Vec<&str> = line.split(" -> ").collect();
-        let from: Vec<u32> = from_to_split
+        let mut from: Vec<i32> = from_to_split
             .get(0)
             .unwrap()
             .split(',')
-            .map(|x| x.parse::<u32>().unwrap())
+            .map(|x| x.parse::<i32>().unwrap())
             .collect();
-        let to: Vec<u32> = from_to_split
+        let to: Vec<i32> = from_to_split
             .get(1)
             .unwrap()
             .split(',')
-            .map(|x| x.parse::<u32>().unwrap())
+            .map(|x| x.parse::<i32>().unwrap())
             .collect();
-        // X equal
-        if from.get(0).unwrap() == to.get(0).unwrap() {
-            let x = *from.get(0).unwrap();
-            // Cant be bothered to compare whats smaller/bigger
-            for n in *from.get(1).unwrap()..=*to.get(1).unwrap() {
-                let value = positions.entry((x, n)).or_insert(0);
-                *value += 1;
+        // Determine growth -1, 0 or 1 for x and y
+        // e.g. 0,9 -> 5,9 => 1,0 per step
+        //      8,0 -> 0,8 => -1,1 per step
+        let x_diff = *to.get(0).unwrap() - *from.get(0).unwrap();
+        let mut x_range: Vec<i32> = vec![-1, 1, x_diff.into()];
+        x_range.sort();
+        let x_change = x_range.get(1).unwrap();
+        let y_diff = *to.get(1).unwrap() - *from.get(1).unwrap();
+        let mut y_range: Vec<i32> = vec![-1, 1, y_diff.into()];
+        y_range.sort();
+        let y_change = y_range.get(1).unwrap();
+        while *from.get(0).unwrap() != (to.get(0).unwrap() + x_change)
+            || *from.get(1).unwrap() != (to.get(1).unwrap() + y_change)
+        {
+            let value = positions
+                .entry((*from.get(0).unwrap(), *from.get(1).unwrap()))
+                .or_insert(0);
+            *value += 1;
+            if let Some(x) = from.get_mut(0) {
+                *x += x_change;
             }
-            for n in *to.get(1).unwrap()..=*from.get(1).unwrap() {
-                let value = positions.entry((x, n)).or_insert(0);
-                *value += 1;
-            }
-        }
-        // Y equal
-        if from.get(1).unwrap() == to.get(1).unwrap() {
-            let y = *from.get(1).unwrap();
-            // Cant be bothered to compare whats smaller/bigger
-            for n in *from.get(0).unwrap()..=*to.get(0).unwrap() {
-                let value = positions.entry((n, y)).or_insert(0);
-                *value += 1;
-            }
-            for n in *to.get(0).unwrap()..=*from.get(0).unwrap() {
-                let value = positions.entry((n, y)).or_insert(0);
-                *value += 1;
+            if let Some(y) = from.get_mut(1) {
+                *y += y_change;
             }
         }
     }
@@ -49,7 +48,7 @@ fn main() {
 }
 
 #[allow(dead_code)]
-fn print_positions(positions: &HashMap<(u32, u32), u32>) {
+fn print_positions(positions: &HashMap<(i32, i32), u32>) {
     let mut highest_x = 0;
     let mut highest_y = 0;
     for pos in positions.keys() {
@@ -60,8 +59,8 @@ fn print_positions(positions: &HashMap<(u32, u32), u32>) {
             highest_y = pos.1;
         }
     }
-    for i in 0..=highest_x {
-        for j in 0..=highest_y {
+    for j in 0..=highest_y {
+        for i in 0..=highest_x {
             if positions.contains_key(&(i, j)) {
                 print!("{:2?}", positions.get(&(i, j)).unwrap());
             } else {
