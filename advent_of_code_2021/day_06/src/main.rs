f5acae81b585b77faab144109a55c4ad92464d88
gitAdrianK <14051526+gitAdrianK@users.noreply.github.com>
2021-12-06 12:08:20 +0100

    Add problem and solution day 06 part 2
    The first "and then we blew up how long it takes"

--- advent_of_code_2021/day_06/src/main.rs
+++ advent_of_code_2021/day_06/src/main.rs
@@ -3,21 +3,35 @@ use std::fs;
 fn main() {
     let mut data = fs::read_to_string("input.txt").expect("Unable to read file");
     data = data.replace("\n", "");
-    let mut fish: Vec<u8> = data
+    let fish: Vec<usize> = data
         .split(',')
-        .map(|x| x.parse::<u8>().unwrap())
+        .map(|x| x.parse::<usize>().unwrap())
         .collect();
-    for _ in 0..80 {
-        let mut spawned: Vec<u8> = Vec::new();
-        for f in &mut fish {
-            if *f == 0 {
-                spawned.push(8);
-                *f = 6;
-            } else {
-                *f -= 1;
-            }
+    // Generate spawn timings
+    let mut can_spawn: Vec<usize> = vec![0; 7];
+    let mut growing_up: Vec<usize> = vec![0; 7];
+    for i in 0..7 {
+        if let Some(x) = can_spawn.get_mut(i) {
+            *x = fish.iter().filter(|&n| *n == i).count();
         }
-        fish.append(&mut spawned);
     }
-    println!("{:?}", fish.len());
+    for day in 0..256 {
+        if day == 79 {
+            print!("{:?} ", can_spawn.iter().sum::<usize>() + growing_up.iter().sum::<usize>());
+        }
+        let today = day % 7;
+        let after_tomorrow = (day + 2) % 7;
+        // Add new spawned fish
+        if let Some(x) = growing_up.get_mut(after_tomorrow) {
+            *x = *can_spawn.get(today).unwrap();
+        }
+        // Add new fish that spawn this day to already spawning fish
+        if let Some(x) = can_spawn.get_mut(today) {
+            *x += *growing_up.get(today).unwrap();
+        }
+        if let Some(x) = growing_up.get_mut(today) {
+            *x = 0;
+        }
+    }
+    println!("{:?}", can_spawn.iter().sum::<usize>() + growing_up.iter().sum::<usize>());
 }
